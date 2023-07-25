68d270f39e546cd0517b795fe9827c8cf6b847d0
Chef <cheftech@patraldo.dev>
2023-07-25 15:13:45 -0600

    install carbon svelte components, etc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+  import { Button, truncate, breakpoints } from "carbon-components-svelte";
+  import { Airplane } from "carbon-pictograms-svelte";
+</script>
+
+<Button>Primary button</Button>
+
+<Airplane />
+
+
 <h1>Welcome to Big Daddy dot Dev</h1>
 
-<p>Not just another wordpress site</p>
+<p>Because Big Data and Go Daddy were already taken.</p>
--- src/routes/wp-api-json/+page.svelte
+++ src/routes/wp-api-json/+page.svelte
@@ -1,18 +1,14 @@
 <script>
-  import Add from "carbon-icons-svelte/lib/Add.svelte";
 
  export let data;
 
 $: ( { media } = data ) 
 console.log(data)
-
-
 </script>
 
-<Add aria-label="Add" tabindex="0" />
-<label id="add-file">Add file</label>
 
-<Add aria-labelledby="add-file" />
+
+
 <h1>Plantas Patraldo Data</h1>
 {#each data.media as media }
 <h2>
@@ -23,6 +19,7 @@ console.log(data)
 <p>{media.id}</p>
 <p>{media.slug}</p>
 {/each}
+
 
 <style>
 h1 {
