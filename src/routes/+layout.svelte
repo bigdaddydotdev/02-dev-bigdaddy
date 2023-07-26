372f3f0f1e3166af439276e363bdbe692aa23d7d
Chef <cheftech@patraldo.dev>
2023-07-26 11:16:52 -0600

    add svelte-confetti

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -54,15 +54,24 @@ active = true;
       <Column>
         <h1>Welcome</h1>
 <button on:click={showConfetti}>Show some confetti</button>
-{#if active}
-<Confetti />
-{/if}
-      </Column>
+     </Column>
     </Row>
   </Grid>
 </Content>
-
-
+{#if active}
+<div style="
+position: fixed;
+top: -50px;
+left: 0;
+height: 100vh;
+width: 100vw;
+display: flex;
+justify-content: center;
+overflow: hidden;
+pointer-events: none;">
+<Confetti x={[-5, 5]} y={[0, 0.1]} delay={[500, 2000]} infinite duration=5000 amount=200 fallDistance="100vh" />
+</div> 
+{/if}
 
 
 
