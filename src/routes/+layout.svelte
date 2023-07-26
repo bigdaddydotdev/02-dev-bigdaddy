d2676bf3c4785e92da19dc1a7669e3a08acac168
Chef <cheftech@patraldo.dev>
2023-07-26 11:20:22 -0600

    add svelte-confetti

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,4 @@
 <script>
-  import { tick } from "svelte";
-  import { Confetti } from "svelte-confetti";
-  let active = false;
-  async function showConfetti() {
-active = false;
-await tick();
-active = true;
-}
-
 
   import "carbon-components-svelte/css/all.css";
   import {
@@ -53,25 +44,10 @@ active = true;
     <Row>
       <Column>
         <h1>Welcome</h1>
-<button on:click={showConfetti}>Show some confetti</button>
      </Column>
     </Row>
   </Grid>
 </Content>
-{#if active}
-<div style="
-position: fixed;
-top: -50px;
-left: 0;
-height: 100vh;
-width: 100vw;
-display: flex;
-justify-content: center;
-overflow: hidden;
-pointer-events: none;">
-<Confetti x={[-5, 5]} y={[0, 0.1]} delay={[500, 2000]} infinite duration=5000 amount=200 fallDistance="100vh" />
-</div> 
-{/if}
 
 
 
