09bd74ebd6989a5d7e76344768422501a53a6c9e
Chef <cheftech@patraldo.dev>
2023-08-03 18:07:14 -0600

    add superforms and zod

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,26 @@
-<script>
+<script lang="ts">
     import "carbon-components-svelte/css/all.css";
   import { Button, truncate, breakpoints } from "carbon-components-svelte";
   import { Airplane } from "carbon-pictograms-svelte";
+
+import { superForm } from "sveltekit-superforms/client"
+	import SuperDebug from "sveltekit-superforms/client/SuperDebug.svelte"
+	import { z } from "zod"
+	import type { PageData } from "./$types"
+
+	export let data: PageData
+
+	const newContactSchema = z.object({
+		firstName: z.string().min(1),
+		lastName: z.string().min(1),
+		email: z.string().email(),
+		company: z.string().min(1)
+	})
+
+	const { form, errors, enhance, constraints } = superForm(data.form, {
+		taintedMessage: "Are you sure you want leave?",
+		validators: newContactSchema
+	})
 </script>
 
 <Button>Primary button</Button>
@@ -13,4 +32,39 @@
 
 <p>Because Big Data and Go Daddy were already taken.</p>
 
+<SuperDebug data={$form} />
+<article>
+	<header>
+		<h1>New Contact</h1>
+	</header>
+	<form method="POST" use:enhance>
+		<label for="firstName">First name</label>
+		<input type="text" id="firstName" name="firstName" bind:value={$form.firstName} />
+		{#if $errors.firstName}
+			<small>{$errors.firstName}</small>
+		{/if}
+		<label for="lastName">Last name</label>
+		<input type="text" id="lastName" name="lastName" bind:value={$form.lastName} />
+		{#if $errors.lastName}
+			<small>{$errors.lastName}</small>
+		{/if}
+		<label for="email">Email address</label>
+		<input type="email" id="email" name="email" bind:value={$form.email} />
+		{#if $errors.email}
+			<small>{$errors.email}</small>
+		{/if}
+		<label for="company">Company</label>
+		<input type="text" id="company" name="company" bind:value={$form.company} />
+		{#if $errors.company}
+			<small>{$errors.company}</small>
+		{/if}
+		<button type="submit">Submit</button>
+	</form>
+</article>
 
+<style>
+	h1
+{
+	margin-bottom: 0 !important;
+}
+</style>
--- src/routes/rss/+page.svelte
+++ src/routes/rss/+page.svelte
@@ -1,34 +1,115 @@
 <script>
-	export let item;
+	import Parser from 'rss-parser/dist/rss-parser.js';
 
-	let contentSnippet;
-	let showAllContent = false;
+	interface IFeedItem {
+		title: string;
+		contentSnippet?: string;
+		content: string;
+		link: string;
+	}
 
-	$: contentSnippet = (item?.contentSnippet || '').split('\n')[0];
+	interface IFeed {
+		title: string;
+		description: string;
+		image?: {
+			url: string;
+		};
+		items: IFeedItem[];
+	}
+
+	const parser = new Parser();
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch }): Promise<{ props: { feed?: IFeed } }> {
+		let props = {};
+
+		const feedParam = page.query.get('feed');
+
+		if (feedParam) {
+			const feedFetch = await fetch(`/feed.xml?feed=${feedParam}`);
+
+			if (feedFetch.ok) {
+				const feedXml = await feedFetch.text();
+				const feed = await parser.parseString(feedXml);
+				props = {
+					feed
+				};
+			}
+		}
+
+		return { props };
+	}
 </script>
 
-<li>
-	<h3>{item.title}</h3>
-	<p>
-		{#if contentSnippet !== item.content}
-			<button on:click|preventDefault={() => (showAllContent = !showAllContent)}
-				>{showAllContent ? 'Read Less' : 'Read More'}</button
-			>
-			{' | '}
+<script type="ts">
+	import { browser } from '$app/env';
+
+	import FeedItem from '../components/FeedItem.svelte';
+
+	export let feed;
+
+	interface Fave {
+		uri: string
+		title: string
+	}
+
+	let favorites: Fave[] = browser ? JSON.parse(localStorage.getItem('recentRssFeeds') ||  '[]') : [];
+
+	$: browser && localStorage.setItem('recentRssFeeds', JSON.stringify(favorites));
+
+	$: {
+		if (feed && browser) {
+			const query = new URLSearchParams(location.search);
+			const feedQuery = query.get('feed');
+
+			const newFavorites = favorites.filter(({uri}) => uri !== feedQuery);
+
+			newFavorites.unshift({uri: feedQuery, title: feed.title});
+
+			favorites = newFavorites.slice(0, 9);
+		}
+	}
+</script>
+
+<h1>RSS Reader</h1>
+<form method="get" action="/">
+	<label>{'RSS Feed:'} <input type="text" name="feed" /></label>
+	<button type="submit">{'Go'}</button>
+</form>
+<div>
+	{#if feed}
+		<a href="/">{'< Return Home'}</a>
+		<h2>{feed.title}</h2>
+		<h4>{feed.description}</h4>
+		{#if feed.image}
+			<img alt={`${feed.title} logo`} style="max-width: 600px;" src={feed.image.url} />
 		{/if}
-		<a href={item.link} target="_blank">{'Open Link'}</a>
-	</p>
-	{#if showAllContent}
-		<p>{@html item.content || ''}</p>
+
+		<ul>
+			{#each feed.items as item}
+				<FeedItem {item} />
+			{/each}
+		</ul>
+
 	{/if}
-	{#if !showAllContent}
-		<p>{showAllContent ? item.contentSnippet : contentSnippet}</p>
+	{#if favorites.length}
+		<h3>Recents</h3>
+		<ul>
+			{#each favorites as favorite}
+				<li>
+					<a href={`/?feed=${favorite.uri}`}>{favorite.title}</a>
+				</li>
+			{/each}
+		</ul>
 	{/if}
-</li>
+</div>
 
 <style>
-	p:nth-child(n + 1) {
-		padding-left: 2rem;
-		border-left: 2px solid #333;
+	h1,
+	form,
+	div {
+		max-width: 800px;
+		margin: 0 auto 2rem;
 	}
 </style>
